9a202909580fdd6580e83913b70c7a5021b28754
RandomiaGaming <randomiagaming@gmail.com>
2024-11-14 16:21:46 -0800

    Final touches

--- NotificationServiceExample/Program.cs
+++ NotificationServiceExample/Program.cs
@@ -6,42 +6,8 @@ using Newtonsoft.Json;
 
 namespace NotificationServiceExample
 {
-    public sealed class NotificationServiceClient
+    public static class Program
     {
-        // Private Variables
-        private int Port = -1;
-        private HttpClient httpClient = null;
-        // Constructor/Destructor
-        public NotificationServiceClient(int port)
-        {
-            Port = port;
-            httpClient = new HttpClient();
-        }
-        ~NotificationServiceClient()
-        {
-            httpClient.Dispose();
-        }
-        // Internal HTTP post helper method
-        private string SendRequest(string endpoint, string inputJson)
-        {
-            string url = $"http://localhost:{Port}/{endpoint}";
-            HttpContent content = new StringContent(inputJson, Encoding.UTF8, "application/json");
-            try
-            {
-                HttpResponseMessage response = httpClient.PostAsync(url, content).GetAwaiter().GetResult();
-                response.EnsureSuccessStatusCode();
-                return response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-            }
-            catch (Exception ex)
-            {
-                return "{\"status\":\"" + ex.Message + "\"}";
-            }
-        }
-        // Json Schemas
-        private sealed class BlankJsonSchema
-        {
-
-        }
         private sealed class StatusJsonSchema
         {
             public string status;
@@ -52,78 +18,31 @@ namespace NotificationServiceExample
             public string message = "";
             public string iconPath = "";
         }
-        // Functions
-        public void Check()
-        {
-            BlankJsonSchema input = new BlankJsonSchema();
-            string inputJson = JsonConvert.SerializeObject(input);
-            string outputJson = SendRequest("Check", inputJson);
-            StatusJsonSchema output = JsonConvert.DeserializeObject<StatusJsonSchema>(outputJson);
-            if (output.status.ToLower() != "OK".ToLower())
-            {
-                throw new Exception(output.status);
-            }
-        }
-        public void Exit()
-        {
-            BlankJsonSchema input = new BlankJsonSchema();
-            string inputJson = JsonConvert.SerializeObject(input);
-            string outputJson = SendRequest("Exit", inputJson);
-            StatusJsonSchema output = JsonConvert.DeserializeObject<StatusJsonSchema>(outputJson);
-            if (output.status.ToLower() != "OK".ToLower())
-            {
-                throw new Exception(output.status);
-            }
-        }
-        public void ShowNotification(string title, string message, string iconPath)
-        {
-            ShowNotificationJsonSchema input = new ShowNotificationJsonSchema();
-            input.title = title;
-            input.message = message;
-            input.iconPath = iconPath;
-            string inputJson = JsonConvert.SerializeObject(input);
-            string outputJson = SendRequest("ShowNotification", inputJson);
-            StatusJsonSchema output = JsonConvert.DeserializeObject<StatusJsonSchema>(outputJson);
-            if (output.status.ToLower() != "OK".ToLower())
-            {
-                throw new Exception(output.status);
-            }
-        }
-
-        // Constant path to NotificationService.exe
-        private const string NotificationServicePath = "D:\\ImportantData\\School\\NotificationService\\NotificationService\\bin\\Release\\NotificationService.exe";
-        // Static methods for launching NotificationService.exe
-        public static int Launch()
-        {
-            Process notificationService = Process.Start(NotificationServicePath);
-            notificationService.WaitForExit(15000);
-            if (!notificationService.HasExited)
-            {
-                throw new Exception("Timeout while waiting for NotificationService.exe.");
-            }
-            if (notificationService.ExitCode <= 0)
-            {
-                throw new Exception("Failed to launch NotificationService.exe.");
-            }
-            else
-            {
-                return notificationService.ExitCode;
-            }
-        }
-        public static void Launch(int port)
-        {
-            Process.Start(NotificationServicePath, "/port " + port.ToString());
-        }
-    }
-    public static class Program
-    {
         public static void Main(string[] args)
         {
-            int port = NotificationServiceClient.Launch();
-            NotificationServiceClient notificationService = new NotificationServiceClient(port);
-            notificationService.Check();
-            notificationService.ShowNotification("Test", "Hello World", "C:\\Users\\RandomiaGaming\\Desktop\\Icon.jpg");
-            notificationService.Exit();
+            // Start NotificationService on a random port
+            Process notificationService = Process.Start("D:\\ImportantData\\School\\NotificationService\\NotificationService\\bin\\Release\\NotificationService.exe");
+            notificationService.WaitForExit();
+            int port = notificationService.ExitCode;
+            // Prepare input for the request
+            ShowNotificationJsonSchema input = new ShowNotificationJsonSchema();
+            input.title = "Test Notification";
+            input.message = "Hello world this is a test notification.";
+            input.iconPath = "C:\\Users\\RandomiaGaming\\Desktop\\Icon.png";
+            string inputJson = JsonConvert.SerializeObject(input);
+            // Send the request
+            HttpClient httpClient = new HttpClient();
+            string url = $"http://localhost:{port}/ShowNotification";
+            HttpContent content = new StringContent(inputJson, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = httpClient.PostAsync(url, content).GetAwaiter().GetResult();
+            response.EnsureSuccessStatusCode();
+            string outputJson = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            // Parse output and ensure status OK
+            StatusJsonSchema output = JsonConvert.DeserializeObject<StatusJsonSchema>(outputJson);
+            if (output.status.ToLower() != "OK".ToLower())
+            {
+                throw new Exception(output.status);
+            }
         }
     }
 }
---
+++ ReadMe.md
@@ -0,0 +1,86 @@
+﻿# NotificationService
+Notification Service is a Windows 10 and 11 executable microservice for displaying notifications.
+The general workflow when using NotificationService is to first launch NotificationService.exe with the desired command line arguments
+then send request with HTTP post.
+
+# Command Line Args
+USAGE: NotificationService.exe [/port:12345]
+
+If the argument /port is provided then the custom port will be used.
+If Notification Service fails to bind to the specified port or if the specified port is invalid then an error will be printed to the console and the process will exit with status code 0.
+If Notification Service successfully binds to the specified port then the microservice will run until sent the exit command at which moment it will exit with status code 0.
+
+If the argument /post is not provided then a random free port will be used.
+Notification Service will find a free port and restart itself with that port as an argument.
+The original parent process will exit with a status code which is the same as the found port.
+For example if NotificationService.exe returns exit code 12345 when launched without the /port argument this means the server is now running in a new process on port 12345.
+
+# Functions
+Each function is called by sending a JSON packet with the specified arguments to a given endpoint.
+If there are no input arguments then an empty JSON object should be used {}.
+For example to call the ShowNotification function I would HTTP post the following JSON to http://localhost:port/ShowNotification
+{
+	"title":"Example Notificaion",
+	"message":"Hello World This Is A Notificaion!",
+	"iconPath":"C:\Users\Me\Desktop\Icon.png"
+}
+NotificationService.exe would then respond with a JSON status in the following form:
+{
+	"status":"OK"
+}
+Or if the function failed the response JSON might look like this:
+{
+	"status":"Unable to show notification because access was denied"
+}
+
+# Check
+The check function always returns status OK as long as NotificationService.exe is running.
+It is used to check that you have the correct port and that NotificationService.exe is running properly.
+Inputs:
+	None
+Outputs:
+	string status // Either OK or an error message
+
+# Exit
+The exit function instructs NotificationService.exe to exit after this request.
+After sending this request no other requests may be sent.
+Inputs:
+	None
+Outputs:
+	string status // Either OK or an error message
+
+# ShowNotification
+The ShowNotificaion function sends the user a windows notification.
+Inputs:
+	string title // The title of the notification.
+	string message // The body aka message of the notification.
+	string iconPath // A fully rooted path to a PNG or JPEG to be used as the notification's icon
+Outputs:
+	string status // Either OK or an error message
+
+# UML Sequence Diagram
+![UML Sequence Diagram](UML.png)
+
+# Mitigation Plan
+For which teammate did you implement "Microservice A"?
+I (Finlay Christ) implamented Microservice A for Michael Louard.
+
+What is the current status of the microservice? Hopefully, it's done!
+It is completely done with all features working.
+
+If the microservice isn't done, which parts aren't done and when will they be done?
+I beleive that everything is done, however, I am happy to swing back and fix bugs as they are discovered.
+
+How is your teammate going to access your microservice? Should they get your code from GitHub (if so, provide a link to your public or private repo)? Should they run your code locally? Is your microservice hosted somewhere? Etc.
+My microservice is availible in two forms. Either as a portable windows executable (.exe) or as C# source code which can be compiled manually.
+Both are hosted on my public GitHub repo: [https://github.com/RandomiaGaming/NotificationService](https://github.com/RandomiaGaming/NotificationService)
+
+If your teammate cannot access/call YOUR microservice, what should they do? Can you be available to help them? What's your availability?
+Our team has had great communication over Discord. I will continue to be availible for integration questions via our Discord group chat and I will reply to pings within 24 hours.
+
+If your teammate cannot access/call your microservice, by when do they need to tell you?
+As soon as you discover an issue with accessing or calling my microservice feel free to send me a ping on Discord. I may not reply to non-pings as quickly so please send me a ping.
+
+Is there anything else your teammate needs to know? Anything you're worried about? Any assumptions you're making? Any other mitigations / backup plans you want to mention or want to discuss with your teammate?
+I have tried my best to create a microservice which will be easy to integrate with since it uses http post and json which are easily accessible in many programming languages.
+I hope that integration will be a smooth process and if not I am happy to assist in any way I can.
